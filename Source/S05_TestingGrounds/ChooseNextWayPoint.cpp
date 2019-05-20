f9acf6c309256f6b71739dfa33a899d1c88f2d9c
subotich <suboti.ch@icloud.com>
2019-05-20 22:51:30 +0200

    Converting Blueprint to C++: ChooseNextWayPoint_BP migration to cpp

--- Source/S05_TestingGrounds/ChooseNextWayPoint.cpp
+++ Source/S05_TestingGrounds/ChooseNextWayPoint.cpp
@@ -2,13 +2,31 @@
 
 
 #include "ChooseNextWayPoint.h"
+#include "AIController.h"
+#include "PatrollingGuard.h"	// TODO de-couple
 #include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWayPoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+	// TODO protect against empty patrol routes
+
+	// Get the patrol points (from ChooseNextWayPoint_BP function)
+	auto AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+
+	// Set next waypoint (from ChooseNextWayPoint_BP function)
 	auto BlackboardComponent = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComponent->GetValueAsInt(IndexKey.SelectedKeyName);
-
 	UE_LOG(LogTemp, Warning, TEXT("Waypoint index: %i"), Index);
+	UE_LOG(LogTemp, Warning, TEXT("Waypoint name: %s"), *WaypointKey.SelectedKeyName.ToString());
+	BlackboardComponent->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	// Cycle index (from ChooseNextWayPoint_BP function)
+	UE_LOG(LogTemp, Warning, TEXT("Patrol points size: %i"), PatrolPoints.Num());
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComponent->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+
 	return EBTNodeResult::Succeeded;
 }
--- Source/S05_TestingGrounds/ChooseNextWayPoint.h
+++ Source/S05_TestingGrounds/ChooseNextWayPoint.h
@@ -20,4 +20,7 @@ protected:
 	UPROPERTY(EditAnywhere, Category = "Blackboard")
 	struct FBlackboardKeySelector IndexKey;
 
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector WaypointKey;
+
 };
