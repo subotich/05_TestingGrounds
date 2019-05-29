3916824b1d016c745798a21ec9b990ae35877319
subotich <suboti.ch@icloud.com>
2019-05-29 09:03:57 +0200

    Holding a Gun with Skeletal Sockets: Moved NPCs BP classes to NPC/Behaviour folder; Updated AThirdPersonGameMode - set default pawn class to our Blueprinted character;

--- Source/S05_TestingGrounds/ThirdPersonGameMode.cpp
+++ Source/S05_TestingGrounds/ThirdPersonGameMode.cpp
@@ -7,7 +7,7 @@
 AThirdPersonGameMode::AThirdPersonGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/NPC/Character/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/NPC/Behaviour/ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
