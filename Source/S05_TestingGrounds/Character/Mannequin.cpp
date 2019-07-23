5b09d21811d2c20da92601afd9438a658058dc0d
subotich <suboti.ch@icloud.com>
2019-07-23 07:38:42 +0200

    Animation Montages and Slots: How the mess all your animations; Made your code unusable; Waste time, repeat & run in circles;

--- Source/S05_TestingGrounds/Character/Mannequin.cpp
+++ Source/S05_TestingGrounds/Character/Mannequin.cpp
@@ -43,7 +43,9 @@ void AMannequin::BeginPlay()
 	}
 	Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
 	Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint")); //Attach gun mesh component to Skeleton, doing it here because the skelton is not yet created in the constructor
-	Gun->AnimInstance = Mesh1P->GetAnimInstance();
+	//Gun->AnimInstance = Mesh1P->GetAnimInstance();
+	Gun->AnimInstance = GetMesh()->GetAnimInstance(); // this is because TP & FP obviously cannot go together from single class
+	// TODO has to be split and refactored, same for the gun & it's animations
 
 	// Challenged and moved to SetupPlayerInputComponent
 	/*
