359cd95fd3068efbcdd30c51895814694cf263cb
subotich <suboti.ch@icloud.com>
2019-07-28 19:21:14 +0200

    Spawning Actors in the Tile: updates method PlaceActors and execute it BP's event graph

--- Source/S05_TestingGrounds/Terrain/Tile.cpp
+++ Source/S05_TestingGrounds/Terrain/Tile.cpp
@@ -11,18 +11,18 @@ ATile::ATile()
 
 }
 
-void ATile::PlaceActors()
-{
+void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn) {
 	FVector Min(0, -2000, 0);
 	FVector Max(4000, 2000, 0);
 	FBox Bounds(Min, Max);
-
-	for (size_t i = 0; i < 20; i++)
+	int NumberToSpawn = FMath::RandRange(MinSpawn, MaxSpawn);
+	for (size_t i = 0; i < NumberToSpawn; i++)
 	{
 		FVector SpawnPoint = FMath::RandPointInBox(Bounds);
-		UE_LOG(LogTemp, Warning, TEXT("SpawnPoint is: %s"), *SpawnPoint.ToCompactString());
+		AActor* Spawned = GetWorld()->SpawnActor<AActor>(ToSpawn);
+		Spawned->SetActorRelativeLocation(SpawnPoint);
+		Spawned->AttachToActor(this, FAttachmentTransformRules(EAttachmentRule::KeepRelative, false));
 	}
-
 }
 
 // Called when the game starts or when spawned
